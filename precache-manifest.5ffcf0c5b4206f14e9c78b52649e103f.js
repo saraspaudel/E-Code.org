8ca6aee4f2b2844e01dd2495d45cae5582ff11b2
Saras Paudel <saraspaudel@utexas.edu>
2019-08-04 23:37:08 -0500

    Updates

---
+++ precache-manifest.5ffcf0c5b4206f14e9c78b52649e103f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f398861613eb8e9cd0e0a4bfa3bb57fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20d55382af56bf310e09",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "8fb1c46dce7d60322c84",
+    "url": "/static/js/2.1f108a3f.chunk.js"
+  },
+  {
+    "revision": "20d55382af56bf310e09",
+    "url": "/static/js/main.20bbd49d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
