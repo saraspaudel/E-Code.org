d1b582778bed0af14c12b52d81c851a1e0dc340a
Saras Paudel <saraspaudel@utexas.edu>
2020-01-09 12:37:58 -0600

    Revert "Updates" commit to bring back CNAME
    This reverts commit ccc638c4b479c349d477dd2f20fae3885fdbddb8.

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.595bffe1e99ce1fb7e2776e0283e0653.js"
+  "./precache-manifest.755f56694f71cf1543133763bb871c94.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
