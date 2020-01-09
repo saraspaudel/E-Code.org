9ecdc0c4c02aba1348d1920755d058b287a7a586
Saras Paudel <saraspaudel@utexas.edu>
2020-01-09 12:36:40 -0600

    Fixed page being cutoff at bottom on mobile

--- src/App.js
+++ src/App.js
@@ -5,6 +5,6 @@ import ComingSoon from 'react-coming-soon';
 
 export default function App() {
   return (
-    <ComingSoon title="E-CoDE (Empowerment through Computer Donation and Education)" subtitle="KIDS (Komputers for Developing Schools) is becoming E-CoDE. Coming soon:" bgColor="#0693e3" textColor="#fafafa" date="Sat Aug 31 2019 00:00:00 GMT-0500 (Central Daylight Time)" illustration="development" />
+    <ComingSoon title="E-CoDE (Empowerment through Computer Donation and Education)" subtitle="KIDS (Komputers for Developing Schools) is becoming E-CoDE. Website and updates coming soon..." bgColor="#0693e3" textColor="#fafafa" illustration="development" />
   );
 }
