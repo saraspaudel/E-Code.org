ccc638c4b479c349d477dd2f20fae3885fdbddb8
Saras Paudel <saraspaudel@utexas.edu>
2020-01-09 12:27:50 -0600

    Updates

---
+++ precache-manifest.595bffe1e99ce1fb7e2776e0283e0653.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d96c4c4d5adc41a2f78dc5bd1a2159e6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "598161ef616ff01ea920",
+    "url": "./static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "5f906de35628b3c091b3",
+    "url": "./static/js/2.3d769734.chunk.js"
+  },
+  {
+    "revision": "67935b6d863b3e23bc08cf31dbc40ecc",
+    "url": "./static/js/2.3d769734.chunk.js.LICENSE"
+  },
+  {
+    "revision": "598161ef616ff01ea920",
+    "url": "./static/js/main.076c72a5.chunk.js"
+  },
+  {
+    "revision": "86d7481e0fca32a934bf",
+    "url": "./static/js/runtime-main.a5ed12d2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
